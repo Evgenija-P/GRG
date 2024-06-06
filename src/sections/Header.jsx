7d80ba617b2624885c2acdb345a9713d4c83ff91
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2024-06-06 16:32:50 +0300

    add 2 reports

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-20 xl:h-[100px] py-[14px] bg-red z-[80]">
       <div className="wrapper h-full flex items-center justify-center">
-        <h2 className="header__title text-white uppercase text-center">ТОВ «ДТС- Харків»</h2>
+        <h2 className="header__title text-white uppercase text-center">ТОВ «ДТС-Харків»</h2>
       </div>
     </header>
   );
