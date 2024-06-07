32501d92c6095e1fe5d244fa6dde0ce34b4bab1f
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2024-06-07 14:31:00 +0300

    reports list

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,10 +2,10 @@ import FooterImage from "/images/stripes.png";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-fit pt-[14px] md:pt-[20px]  bg-red">
+    <footer className="w-full h-fit pt-[14px] md:pt-[20px] bg-red">
       <div className="mx-auto h-[40px] md:h-[60px] flex items-center justify-center mb-[10px]">
         <h2 className="text-xs md:text-base xl:text-lg uppercase font-bold text-white">
-          ТОВ «ДТС- Харків»
+          ТОВ «ДТС-Харків»
         </h2>
       </div>
       <div className="w-full h-[30px] md:h-[50px] flex justify-center">
